ec792cc55dfe8e7574fe90a5396b0674bc171af3
Daniel <danicoetzee1@gmail.com>
2023-07-24 19:52:09 +0200

    progress

--- CSGOMatchExtractor.py
+++ CSGOMatchExtractor.py
@@ -1,31 +1,68 @@
 import requests
+import os
+from dotenv import load_dotenv
+import time
 
 
-def MakeAPIRequest(URL, API_KEY):
+def FetchMatchList(API_KEY, HubIDList, MatchLimit):
+    matchIDList = []
+    offset = 0
+    LIMIT = 100
+    iterationAmount = 0
 
     headers = {
             'Authorization': f'Bearer {FACEIT_API_KEY}'
     }
 
-    try:
-        response = requests.get(HUB_INFO_URL, headers=headers)
+    for hub in HubIDList:
+        print(f"Fetching match IDs: {hub}")
 
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error occured: {e}")
+        while (offset < MatchLimit):
+            requestSuccess = False
+            URL = f"https://open.faceit.com/data/v4/hubs/{hub}/matches?offset={offset}&limit={LIMIT}"
+            iterationAmount += 1
+            print(f"Iteration number {iterationAmount}, matches processed {len(matchIDList)}")
+
+            while (requestSuccess == False):
+                try:
+                    response = requests.get(URL, headers=headers)
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        matchList = data["items"]
+
+                        for match in matchList:
+                            matchID = match["match_id"]
+                            matchIDList.append(matchID)
+                        requestSuccess = True
+                    else:
+                        print(f"Request failed with status code: {response.status_code}")
+                        time.sleep(5)
+                except requests.exceptions.RequestException as e:
+                    print(f"Error occured: {e}")
+            
+            offset += LIMIT
+
+    print("-------------PROCESSING COMPLETE--------------")
+    print(f"Total number of matches fetched --> {len(matchIDList)}")
+    return matchIDList
+
+def MatchIDListSaver(matchIDList):
+    print("Writing match IDs to disk")
+    with open("MatchIDList.txt", 'w') as file:
+        for matchID in matchIDList:
+            file.write(f"{matchID}\n")
+
 
 
 if __name__ == "__main__":
-    FACEIT_API_KEY = "5f6ab6e6-9e7c-4457-b377-7dbb9b05f16e"
-    HUB_INFO_URL = 'https://open.faceit.com/data/v4/hubs/4531cb6b-7dd5-4492-aef5-f092648c072f/matches?offset=0&limit=20'
+    load_dotenv()
 
-    apiResponse = MakeAPIRequest(HUB_INFO_URL, FACEIT_API_KEY)
+    FACEIT_API_KEY = os.getenv('FACEIT_API_KEY')
+    HUB_ID_LIST_ENV = os.getenv('FACEIT_HUB_ID_LIST')
 
-    matchList = apiResponse["items"]
+    HUB_ID_LIST = HUB_ID_LIST_ENV.split(',')
 
-    for match in matchList:
-        print(match["match_id"])
+    matchIDList = FetchMatchList(FACEIT_API_KEY, HUB_ID_LIST, 50000)
+
+    MatchIDListSaver(matchIDList)
