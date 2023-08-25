0c37930f17345df032c8a9999c8ffe9f0d2e54aa
Daniel <danicoetzee1@gmail.com>
2023-08-25 15:10:21 +0200

    A bunch of exploration

--- CSGODemo/DemoDataset.py
+++ CSGODemo/DemoDataset.py
@@ -3,22 +3,44 @@ import os
 from dotenv import load_dotenv
 import csv
 
+def calculate_team_averages(match_info, match, cursor):
+    team1_stats = match_info[:70]  # Assuming the first 70 elements are for Team 1 stats
+    team2_stats = match_info[70:140]  # Assuming elements 71 to 140 are for Team 2 stats
+
+    # Calculate averages for Team 1 stats
+    team1_averages = [sum([x for x in team1_stats[i::14] if x is not None]) / len([x for x in team1_stats[i::14] if x is not None]) for i in range(14)]
+
+    # Calculate averages for Team 2 stats
+    team2_averages = [sum([x for x in team2_stats[i::14] if x is not None]) / len([x for x in team2_stats[i::14] if x is not None]) for i in range(14)]
+
+    matchOutcome, scoreDifference, closeMatch = getMatchResult(match, cursor)
+
+    fullStats = team1_averages + team2_averages
+    fullStats.append(matchOutcome)
+    fullStats.append(scoreDifference)
+    fullStats.append(closeMatch)
+    return fullStats
+
 def MatchProcessor(match, cursor):
     team1Players = GetAllPlayersForMatch(match, 1, cursor)
     team2Players = GetAllPlayersForMatch(match, 2, cursor)
 
     team1Stats = GetTeamStats(team1Players, match, cursor)
     team2Stats = GetTeamStats(team2Players, match, cursor)
-    matchOutcome = getMatchResult(match, cursor)
+    matchOutcome, scoreDifference, closeMatch = getMatchResult(match, cursor)
 
     fullMatchStats = team1Stats + team2Stats
     fullMatchStats.append(matchOutcome)
+    fullMatchStats.append(scoreDifference)
+    fullMatchStats.append(closeMatch)
 
     return fullMatchStats
 
 
 def getMatchResult(match, cursor):
     team1Winner = 1
+    scoreDifference = 0
+    closeMatch = 0
 
     matchScoresQuery = f"""
         SELECT Team_1_Score, Team_2_Score
@@ -30,15 +52,19 @@ def getMatchResult(match, cursor):
 
     team1Score = queryResult[0]
     team2Score = queryResult[1]
+    scoreDifference = abs(team1Score - team2Score)
+
+    if scoreDifference <= 3:
+        closeMatch = 1
 
     if (team1Score > team2Score):
-        return team1Winner #Team 1 won the match
+        return team1Winner, scoreDifference, closeMatch #Team 1 won the match
     elif (team1Score == team2Score):
         team1Winner = 2 #Team 1, draw or faulty statistic
-        return team1Winner
+        return team1Winner, scoreDifference, closeMatch
     else:
         team1Winner = 0 #Team 1 lost the match
-        return team1Winner
+        return team1Winner, scoreDifference, closeMatch
 
 
 def GetTeamStats(team, match, cursor):
@@ -212,8 +238,27 @@ if __name__ == "__main__":
         CLASSIFICATION_HEADER.extend(player_columns)
     
     CLASSIFICATION_HEADER.append("Team_1_Win?")
+    CLASSIFICATION_HEADER.append("ScoreDifference")
+    CLASSIFICATION_HEADER.append("closeMatch?")
 
     CSVCreator(CLASSIFICATION_HEADER, "CSGODataset.csv")
+
+    AVERAGE_CLASSIFICATION_HEADER = []
+
+    # Generate column names for Team 1 average stats
+    for stat in CLASSIFICATION_HEADER[:14]:  # Assuming the first 14 columns are for Team 1 players
+        team1_stat = f"Team1_Avg_{stat.split('_')[2]}"  # Extracting the stat name after the second underscore
+        AVERAGE_CLASSIFICATION_HEADER.append(team1_stat)
+
+    # Generate column names for Team 2 average stats
+    for stat in CLASSIFICATION_HEADER[14:28]:  # Assuming columns 15 to 28 are for Team 2 players
+        team2_stat = f"Team2_Avg_{stat.split('_')[2]}"  # Extracting the stat name after the second underscore
+        AVERAGE_CLASSIFICATION_HEADER.append(team2_stat)
+
+    # Add the target columns
+    AVERAGE_CLASSIFICATION_HEADER.extend(["Team_1_Win?", "ScoreDifference", "closeMatch?"])
+
+    CSVCreator(AVERAGE_CLASSIFICATION_HEADER, 'AveragedDataset.csv')
 
     matchList = GetAllMatches(cursor)
     progress = 0
@@ -222,5 +267,8 @@ if __name__ == "__main__":
 
         print(f"UPDATING MATCH - {progress} OUT OF {len(matchList)} [{match}]")
         matchInfo = MatchProcessor(match, cursor)
+        teamAverages = calculate_team_averages(matchInfo, match, cursor)
+        print(teamAverages)
         CSVCreator(matchInfo, "CSGODataset.csv")
+        CSVCreator(teamAverages, 'AveragedDataset.csv')
     
